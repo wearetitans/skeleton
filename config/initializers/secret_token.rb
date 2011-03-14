7d1568a648524928842793937545ece6dc535980
Andrew Culver <andrew.culver@gmail.com>
2011-03-14 11:56:01 -0400

    First commit of empty Rails (3.0.5) project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skeleton::Application.config.secret_token = 'aff1b33316295354f7463493d22a03750f4efe34c683b9202050d9b25af4121a4d3ceee9bc800ae2baa3e916f76873a8b9adf1c5c92f3416e3ac1975973ea33e'
